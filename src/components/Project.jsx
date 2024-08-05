d51d1283876050d3fdeceb6ce85af720d1e17a0a
Ayushi Behl <ayushi.behl94@gmail.com>
2024-08-05 17:29:45 -0400

    Added the project and projects component

---
+++ src/components/Project.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Project = ({ imageTablet, imageMobile, image, title, imageAltText, description, tags }) => {
+  return (
+    <div className="w-full flex xs:flex-row flex-col bg-shade-of-dark-blue border-2 border-shade-of-light-blue md:rounded-[1.25rem] xs:rounded-[0.75rem] rounded-[0.5rem] xs:gap-x-[1.5rem] gap-y-[0.5rem] md:p-[1.5rem] xs:p-[0.75rem] p-[0.5rem]">
+      <div className="flex justify-center items-center flex-col">
+        <picture className="flex justify-center items-center">
+          <source srcSet={imageTablet} media="(min-width: 321px) and (max-width: 768px)" />
+          <source srcSet={imageMobile} media="(max-width: 320px)" />
+          <img
+            src={image}
+            alt={imageAltText}
+            className="md:rounded-[1.25rem] xs:rounded-[0.625rem] rounded-[0.75rem] border border-[#D9D9D9] shadow-md max-w-full h-auto"
+          />
+        </picture>
+      </div>
+
+      <div className="flex flex-col justify-center w-full gap-y-[0.5rem] md:gap-y-[1.125rem]">
+        <div className="md:text-[1.5rem] font-semibold text-shade-of-white text-[0.75rem] xs:text-left text-center">
+          {title}
+        </div>
+        <div className="md:text-[1.25rem] font-normal text-shade-of-white text-[0.75rem]">
+          {description}
+        </div>
+        <div className="flex items-center xs:justify-start justify-around flex-wrap md:gap-x-[1.5rem] gap-y-[0.5rem] gap-x-[1rem]">
+          {tags.map((tag, index) => (
+            <button
+              key={index}
+              className="bg-shade-of-project-tags text-shade-of-white md:text-[0.875rem] font-medium xs:py-[0.25rem] md:px-[1.5rem] md:rounded-[0.5rem] shadow-md border-none text-[0.5rem] xs:px-[1rem] rounded-[0.25rem] px-[0.5rem]"
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+        <div className="flex items-center xs:gap-x-[0.5rem] gap-[0.25rem]">
+          <span className="md:text-[1rem] font-semibold text-shade-of-white text-[0.5rem]">View Project</span>
+          <picture className="flex justify-center items-center">
+            <source srcSet="./images/project-section/arrow-icon-tablet.svg" media="(min-width: 321px) and (max-width: 768px)" />
+            <source srcSet="./images/project-section/arrow-icon-mobile.svg" media="(max-width: 320px)" />
+            <img src="./images/project-section/arrow-icon.svg" alt="Arrow icon" className="max-w-full h-auto" />
+          </picture>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,67 @@
-export default function Projects(){
-    return (
-        <>
-        </>
-    );
+import React from 'react';
+import Project from '../components/Project';
+
+const mockData = [
+  {
+    imageTablet: "./images/project-section/canada-road-ready-image-tablet.jpeg",
+    imageMobile: "./images/project-section/canada-road-ready-image-mobile.jpeg",
+    image: "./images/project-section/canada-road-ready-image.jpeg",
+    title: "Canada Road Ready",
+    description: "Learning app to prepare for driving test in Canada. It is for people to practice and get familiar with the G1 exam.",
+    tags: ["Javascript", "CSS", "HTML"],
+    imageAltText: "Car road exam evaluation"
+  },
+  {
+    imageTablet: "./images/project-section/online-bakery-image-tablet.jpeg",
+    imageMobile: "./images/project-section/online-bakery-image-mobile.jpeg",
+    image: "./images/project-section/online-bakery-image.jpeg",
+    title: "Online Bakery App",
+    description: "An online bakery, implemented into a mobile application. This project was entirely created using Figma.",
+    tags: ["Figma", "Prototypes", "Wireframes"],
+    imageAltText: "An image of a bakery shop"
+  },
+  {
+    imageTablet: "./images/project-section/google-calendar-image-tablet.jpeg",
+    imageMobile: "./images/project-section/google-calendar-image-mobile.jpeg",
+    image: "./images/project-section/google-calendar-image.jpg",
+    title: "LLD of Google Calendar",
+    description: "The Low Level Design of Google Calendar implemented using Design patterns in Java.",
+    tags: ["Java", "GitHub", "System Design"],
+    imageAltText: "A girl checking out a Calendar"
+  }
+];
+
+export default function Projects() {
+  return (
+    <div>
+      <section
+        className="flex flex-col items-center justify-center md:pt-[3.75rem] md:pb-[6.25rem] bg-gradient-to-b from-[#615EC6] to-[#0A092D] md:gap-y-[3rem] xs:py-[3.75rem] xs:gap-y-[1rem] pt-[3rem] pb-[2.5rem] gap-y-[0.5rem]"
+      >
+        <div className="flex flex-col items-center justify-center w-full">
+          <div className="relative md:text-[2rem] xs:text-[1rem] font-semibold mb-[2rem] text-shade-of-white font-serif xs:mb-[1.25rem]">
+            Projects<span className="text-shade-of-light-blue">.</span>
+            <div className="absolute bottom-[-2px] left-0 w-full h-[0.125rem] bg-shade-of-light-blue rounded-md md:bottom-[-4px]" />
+          </div>
+          <div className="hidden md:text-[1.5rem] xs:text-[0.75rem] font-semibold text-shade-of-white text-center md:mb-[4rem] xs:mb-[2.75rem] xs:w-[70%] xs:block">
+            A collection of my most significant projects, showcasing skills in design and development
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center md:w-[72%] md:gap-y-[1.875rem] xs:w-[72%] xs:gap-y-[1rem] xs:w-[80%] md:row-gap-[1rem] w-[72%] gap-y-[0.75rem]">
+          {mockData.map((project, index) => (
+            <Project
+              key={index}
+              imageTablet={project.imageTablet}
+              imageMobile={project.imageMobile}
+              image={project.image}
+              title={project.title}
+              description={project.description}
+              tags={project.tags}
+              imageAltText={project.imageAltText}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
