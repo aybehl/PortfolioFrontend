6db9a29577722433e7625737f3ae93a231cb8041
Ayushi Behl <ayushi.behl94@gmail.com>
2024-08-06 20:58:27 -0400

    Added POST API logic to submit the contact form

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,6 +1,76 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function ContactMe() {
+  const [ formData, setFormData ] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const [ errors, setErrors ] = useState({});
+  const [ apiResponse, setApiResponse ] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    //console.log(e.target);
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const validateForm = () => {
+    let formErrors = {};
+    if(!formData.name){
+      formErrors.name = "Name is required";
+    }
+
+    if(!formData.email){
+      formErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      formErrors.email = 'Email address is invalid';
+    }
+
+    return formErrors;
+  };
+
+  const handleSubmit = async (e) => {
+    console.log(process.env.REACT_APP_BACKEND_URL);
+    setErrors({});
+    e.preventDefault();
+
+    const formErrors = validateForm();
+    
+    if(Object.keys(formErrors).length === 0){
+      try {
+        const response = await fetch(`http://localhost:8076/contacts/add`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        });
+
+        if(response.ok){
+          setApiResponse('Form submitted successfully');
+          setFormData({
+            name: '',
+            email: '',
+            message: ''
+          });
+        } else {
+          setApiResponse('Form submission failed');
+        }
+      } catch(error){
+        console.error(`POST request to submit request with error - ${error.message}`);
+        setApiResponse('An error occurred while submitting the form');
+      }
+    } else {
+      setErrors(formErrors);
+    }
+  };
+
   return (
     <div>
       <section
@@ -17,18 +87,40 @@ export default function ContactMe() {
         </div>
 
         <div className="md:border-4 border-shade-of-white rounded-[0.25rem] md:p-[2rem] bg-shade-of-dark-blue md:w-[40%] border-[0.0625rem] p-[0.5rem] w-[80%] xs:w-[60%] xs:p-[1rem] xs:border-[0.125rem]">
-          <form action="#">
+          <form onSubmit={handleSubmit}>
             <div className="md:mb-[2rem] mb-[1rem]">
               <label htmlFor="name" className="font-semibold md:text-[1.25rem] text-shade-of-white text-[0.75rem]">Name</label>
-              <input type="text" id="name" name="name" placeholder="Enter Your Name" className="w-full rounded-[0.25rem] md:p-[1.5rem] md:pr-[5rem] md:mt-[0.75rem] cursor-text text-shade-of-purple md:text-[1.25rem] md:focus:border-[4px] focus:border-shade-of-lighter-purple focus:outline-none text-[0.75rem] p-[0.75rem] pr-[2.5rem] mt-[0.5rem] focus:border-[2px]" />
+              <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                placeholder="Enter Your Name" 
+                value={formData.name}
+                onChange={handleChange}
+                className="w-full rounded-[0.25rem] md:p-[1.5rem] md:pr-[5rem] md:mt-[0.75rem] cursor-text text-shade-of-purple md:text-[1.25rem] md:focus:border-[4px] focus:border-shade-of-lighter-purple focus:outline-none text-[0.75rem] p-[0.75rem] pr-[2.5rem] mt-[0.5rem] focus:border-[2px]" />
+                { errors.name && <p className="text-red-500 text-xs mt-1">{errors.name}</p> }
             </div>
             <div className="md:mb-[2rem] mb-[1rem]">
               <label htmlFor="email" className="font-semibold md:text-[1.25rem] text-shade-of-white text-[0.75rem]">Email</label>
-              <input type="email" id="email" name="email" placeholder="Enter Your Email" className="w-full rounded-[0.25rem] md:p-[1.5rem] md:pr-[5rem] md:mt-[0.75rem] cursor-text text-shade-of-purple md:text-[1.25rem] md:focus:border-[4px] focus:border-shade-of-lighter-purple focus:outline-none text-[0.75rem] p-[0.75rem] pr-[2.5rem] mt-[0.5rem] focus:border-[2px]" />
+              <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                placeholder="Enter Your Email" 
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full rounded-[0.25rem] md:p-[1.5rem] md:pr-[5rem] md:mt-[0.75rem] cursor-text text-shade-of-purple md:text-[1.25rem] md:focus:border-[4px] focus:border-shade-of-lighter-purple focus:outline-none text-[0.75rem] p-[0.75rem] pr-[2.5rem] mt-[0.5rem] focus:border-[2px]" />
+                { errors.email && <p className="text-red-500 text-xs mt-1">{errors.email}</p> }
             </div>
             <div className="md:mb-[2rem] mb-[1rem]">
               <label htmlFor="message" className="font-semibold md:text-[1.25rem] text-shade-of-white text-[0.75rem]">Message</label>
-              <textarea id="message" name="message" placeholder="Enter Your Message" className="w-full resize-none rounded-[0.25rem] md:p-[1.5rem] md:pr-[5rem] md:pb-[5rem] md:mt-[0.75rem] cursor-text text-shade-of-purple md:text-[1.25rem] md:focus:border-[4px] focus:border-shade-of-lighter-purple focus:outline-none text-[0.75rem] p-[0.75rem] pr-[2.5rem] pb-[2.5rem] mt-[0.5rem] focus:border-[2px]" />
+              <textarea 
+                id="message" 
+                name="message" 
+                placeholder="Enter Your Message"
+                value={formData.message}
+                onChange={handleChange}
+                className="w-full resize-none rounded-[0.25rem] md:p-[1.5rem] md:pr-[5rem] md:pb-[5rem] md:mt-[0.75rem] cursor-text text-shade-of-purple md:text-[1.25rem] md:focus:border-[4px] focus:border-shade-of-lighter-purple focus:outline-none text-[0.75rem] p-[0.75rem] pr-[2.5rem] pb-[2.5rem] mt-[0.5rem] focus:border-[2px]" />
             </div>
             <div className="flex justify-end">
               <button type="submit" className="bg-shade-of-purple text-shade-of-white md:py-[1rem] md:px-[2rem] rounded-[0.25rem] uppercase md:text-[1.25rem] font-semibold border border-[#484646] py-[0.5rem] px-[1rem] text-[0.75rem]">
@@ -36,6 +128,7 @@ export default function ContactMe() {
               </button>
             </div>
           </form>
+          { apiResponse && <div className="mt-4 text-center text-shade-of-white">{apiResponse}</div>}
         </div>
       </section>
     </div>
